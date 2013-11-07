d4e33f95a18e0620cba5b3f298cdb46b58c5893e
Erica Kwan <erica@codeforamerica.org>
2013-11-06 17:40:00 -0800

    Want to show this in an iframe on office displays

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :allow_iframe_requests
+
   include IPWhitelistedController
   protect_from_forgery
 
+private
+
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
+  end
 end
