5e389d9f2d20d4a9a9657fd8adc263d35a016bc5
Erica Kwan <erica@codeforamerica.org>
2014-02-24 14:49:06 -0800

    Features weren't doing well, didn't want to deal with the silliness
    - I'm okay with deleting the features because they were very minimal
    - Add code to the projects factories

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :project, class: JenkinsProject do
     name { Faker::Name.name }
+    code { Faker::Name.name }
     repo_name "project"
     jenkins_base_url "http://www.example.com"
     jenkins_build_name "project"
@@ -10,6 +11,7 @@ FactoryGirl.define do
 
   factory :travis_project, class: TravisProject do
     name { Faker::Name.name }
+    code { Faker::Name.name }
     repo_name "project"
     travis_github_account "account"
     travis_repository "project"
@@ -17,12 +19,14 @@ FactoryGirl.define do
 
   factory :cruise_control_project, class: CruiseControlProject do
     name { Faker::Name.name }
+    code { Faker::Name.name }
     repo_name "project"
     cruise_control_rss_feed_url "http://www.example.com/project.rss"
   end
 
   factory :team_city_project, class: TeamCityProject do
     name { Faker::Name.name }
+    code { Faker::Name.name }
     repo_name "project"
     team_city_base_url "foo.bar.com:1234"
     team_city_build_id "bt567"
@@ -30,6 +34,7 @@ FactoryGirl.define do
 
   factory :team_city_rest_project, class: TeamCityRestProject do
     name { Faker::Name.name }
+    code { Faker::Name.name }
     repo_name "project"
     team_city_rest_base_url "example.com"
     team_city_rest_build_type_id "bt456"
@@ -37,12 +42,14 @@ FactoryGirl.define do
 
   factory :semaphore_project, class: SemaphoreProject do
     name { Faker::Name.name }
+    code { Faker::Name.name }
     repo_name "project"
     semaphore_api_url 'https://semaphoreapp.com/api/v1/projects/2fd4e1c67a2d28fced849ee1bb76e7391b93eb12/123/status?auth_token=nyargh'
   end
 
   factory :tddium_project, class: TddiumProject do
     name { Faker::Name.name }
+    code { Faker::Name.name }
     repo_name "project"
     tddium_auth_token 'b5bb9d8014a0f9b1d61e21e796d78dccdf1352f2'
     tddium_project_name 'Test Project A'
@@ -50,6 +57,7 @@ FactoryGirl.define do
 
   factory :circleci_project, class: CircleCiProject do
     name { Faker::Name.name }
+    code { Faker::Name.name }
     repo_name "project"
     circleci_auth_token 'b5bb9d8014a0f9b1d61e21e796d78dccdf1352f2'
     circleci_project_name 'a-project'
