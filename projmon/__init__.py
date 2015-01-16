cce4632322fc196f48dce7302dd376e84f933ffe
Michal Migurski <mike@teczno.com>
2015-01-16 15:59:44 -0800

    Added stubby HTML for projects view

--- projmon/__init__.py
+++ projmon/__init__.py
@@ -1,4 +1,18 @@
-from os.path import join, dirname
+from os.path import join, dirname, realpath
+import json
+
+from flask import Flask, render_template
 
 with open(join(dirname(__file__), 'VERSION')) as file:
     __version__ = file.read().strip()
+
+PROJECTS_FILE = realpath(join(dirname(__file__), '..', 'projects.json'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    with open(PROJECTS_FILE) as file:
+        projects = json.load(file)
+
+    return render_template('index.html', projects=projects)
