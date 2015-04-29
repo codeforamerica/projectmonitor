b4ef1c10e21270a05d2fce7530bd762b56b72504
Andrew Hyder <hyd415@gmail.com>
2015-04-28 20:00:35 -0700

    Project monitor now works dynamically off the cfapi projects

--- projmon/__init__.py
+++ projmon/__init__.py
@@ -1,142 +1,64 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, division, print_function
+from __future__ import print_function
 from future import standard_library; standard_library.install_aliases()
 
-from os.path import join, dirname, realpath
-from itertools import groupby
-from operator import itemgetter
 from urllib.parse import urlparse
-from csv import DictReader
-from time import time
-import json, sys, os
 
 from requests import get
-from psycopg2 import connect
-from psycopg2.extras import DictCursor
-from flask import Flask, request, render_template, jsonify
-
-with open(join(dirname(__file__), 'VERSION')) as file:
-    __version__ = file.read().strip()
-
-PROJECTS_FILE = realpath(join(dirname(__file__), '..', 'projects.json'))
-STATUSES_FILE = realpath(join(dirname(__file__), '..', 'statuses.csv'))
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    with open(PROJECTS_FILE) as file:
-        projects = json.load(file)
 
-    with connect(os.environ['DATABASE_URL']) as conn:
-        with conn.cursor(cursor_factory=DictCursor) as db:
-            db.execute('''SELECT guid, success, url, updated_at, valid_readme
-                          FROM statuses WHERE updated_at IS NOT NULL
-                          ORDER BY guid''')
+@app.route('/<cfapi_org_id>/')
+def organizations_projects(cfapi_org_id):
 
-            statuses = map(dict, db.fetchall())
+    # Load 10 of this groups projects from CfAPI
+    def get_projects(projects, url, limit=10):
+        got = get(url)
+        print(url, limit)
+        new_projects = got.json()["objects"]
+        projects = projects + new_projects
+        if limit:
+            if len(projects) >= limit:
+                return projects
+        if "next" in got.json()["pages"]:
+            projects = get_projects(projects, got.json()["pages"]["next"], limit)
+        return projects
 
-    for (guid, group) in groupby(statuses, itemgetter('guid')):
-        statuses = sorted(group, key=itemgetter('updated_at'), reverse=True)
+    travis_projects = []
+    projects = []
+    projects = get_projects(projects, "https://www.codeforamerica.org/api/organizations/"+cfapi_org_id+"/projects", 5)
 
-        for status in statuses:
-            status['state_class'] = 'success' if status['success'] else 'failure error'
-            status['state_label'] = u'✓' if status['success'] else u'❌'
+    # Loop through projects and get
+    for project in projects:
+        if project["code_url"]:
+            url = urlparse(project["code_url"])
+            if url.netloc == "github.com":
+                travis_url = "https://api.travis-ci.org/repositories"+url.path+"/builds"
 
-        project = [proj for proj in projects if proj['guid'] == guid][0]
-        project['updated_at'] = statuses[0]['updated_at'].strftime('%Y-%m-%dT%H:%M:%SZ')
-        project['valid_readme'] = statuses[0]['valid_readme']
-        project['state_class'] = statuses[0]['state_class']
-        project['statuses'] = statuses[:5]
+                got = get(travis_url)
+                builds = got.json()
+                if builds:
+                    project["statuses"] = builds[0:5]
 
-    projects = [proj for proj in projects if 'updated_at' in proj]
-    projects.sort(key=itemgetter('updated_at'), reverse=True)
+                    if builds[0]["result"] == 0:
+                        project["state_class"] = "success"
+                    else:
+                        project["state_class"] = "failure error"
 
-    return render_template('index.html', projects=projects)
+                    for status in project["statuses"]:
+                        if status["result"] == 0:
+                            status["state_class"] = "success"
+                            status["state_label"] = unicode("✓", "utf8")
+                        else:
+                            status["state_class"] = "failure error"
+                            status["state_label"] = unicode("❌", "utf8")
+                        status["url"] = "https://travis-ci.org/"+url.path+"/builds/"+str(status["id"])
 
-@app.route('/projects/<guid>/status', methods=['POST'])
-def post_status(guid):
-    try:
-        if 'payload' not in request.form:
-            raise RuntimeError('Missing payload')
+                    travis_projects.append(project)
 
-        payload = json.loads(request.form['payload'])
-        build_url = payload.get('build_url', '')
-        project = None
 
-        with open(PROJECTS_FILE) as file:
-            for other_project in json.load(file):
-                if other_project['guid'] != guid:
-                    continue
-                if build_url.startswith(other_project['travis url']):
-                    project = other_project
 
-        if not project:
-            raise RuntimeError('No match found for {}, {}'.format(guid, build_url))
-
-        _, _, build_path, _, _, _ = urlparse(build_url)
-        info_url = 'https://api.travis-ci.org{}'.format(build_path)
-        print('info_url:', info_url, file=sys.stderr)
-
-        got = get(info_url)
-
-        if got.status_code != 200:
-            raise RuntimeError('HTTP {} for {}'.format(got.status_code, info_url))
-
-        info = got.json()
-        success = (info['status'] == 0)
-        updated_at = info.get('finished_at', info['started_at'])
-        valid_readme = None
-
-        with connect(os.environ['DATABASE_URL']) as conn:
-            with conn.cursor(cursor_factory=DictCursor) as db:
-                db.execute('''INSERT INTO statuses
-                              (guid, success, url, updated_at, valid_readme)
-                              VALUES (%s, %s, %s, %s, %s)''',
-                           (project['guid'], success, build_url, updated_at, True))
-
-        print('post_status: guid={guid}, success={success}, url={build_url}, updated_at={updated_at}, valid_readme=?'.format(**locals()), file=sys.stderr)
-
-    except RuntimeError as e:
-        raise
-
-    else:
-        return 'ok'
-
-@app.route('/.well-known/status')
-def status():
-    with open(PROJECTS_FILE) as file:
-        projects = json.load(file)
-
-    try:
-        for project in projects:
-            _, host, path, _, _, _ = urlparse(project['travis url'])
-            api_url = 'https://api.{host}/repos{path}'.format(**locals())
-            resp = get(api_url)
-
-            # See if the Github URL has moved.
-            if resp.status_code == 404:
-                github_url = 'https://github.com{path}'.format(**locals())
-                resp = get(github_url)
-
-                if resp.status_code == 200:
-                    _, _, github_path, _, _, _ = urlparse(resp.url)
-
-                    if github_path != path:
-                        message = 'Error in {guid}: {path} has moved to {github_path}'
-                        kwargs = dict(guid=project['guid'], **locals())
-                        raise Exception(message.format(**kwargs))
-
-            if resp.status_code != 200:
-                message = 'Missing {guid}: no {travis url}'
-                raise Exception(message.format(**project))
-    except Exception as e:
-        status = str(e)
-    else:
-        status = 'ok'
-
-    return jsonify(dict(status=status,
-                        updated=int(time()),
-                        dependencies=['Travis', 'Github'],
-                        resources={}))
+    return render_template('index.html', projects=travis_projects)
